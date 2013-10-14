dd5beb129d6506898199bca8246b151544bce0e3
tetesting <tetestingtesting@gmail.com>
2013-10-14 19:47:07 -0400

    complete voting form and functionality

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,6 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
+from django.core.urlresolvers import reverse
 
 from polls.models import Poll
 
@@ -15,9 +16,25 @@ def detail(request, poll_id):
     poll = get_object_or_404(Poll, pk=poll_id)
     return render(request, 'polls/detail.html', {'poll':poll})
 
-def results(request, poll_id):
-    return HttpResponse('You\'re looking at the results of poll %s.' % poll_id)
-
 def vote(request, poll_id):
-    return HttpResponse('You\'re voting on poll %s.' % poll_id)
+    poll = get_object_or_404(Poll, pk=poll_id)
+    try:
+        selected_choice = poll.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the poll voting form.
+        return render(request, 'polls/detail.html', {
+            'poll': poll,
+            'error_message': 'You didn\'t select a choice.',
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse('polls:results', args=(poll.id,)))
+
+def results(request, poll_id):
+    poll = get_object_or_404(Poll, pk=poll_id)
+    return render(request, 'polls/results.html', {'poll':poll})
 
