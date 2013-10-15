461d9b130ec53c08c8ee94c4790732b227650ce6
tetesting <tetestingtesting@gmail.com>
2013-10-14 21:27:30 -0400

    add tests for poll:index view

--- polls/tests.py
+++ polls/tests.py
@@ -41,3 +41,60 @@ class PollMethodTests(TestCase):
         recent_poll = create_poll(question="Recent poll.", days=-0.5)
         self.assertEqual(recent_poll.was_published_recently(), True)
 
+
+class PollViewTests(TestCase):
+    def test_index_view_with_no_polls(self):
+        """
+        If no polls exist, an appropriate message should be displayed.
+        """
+        response = self.client.get(reverse('polls:index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "No polls are available.")
+        self.assertQuerysetEqual(response.context['poll_list'], [])
+
+    def test_index_view_with_a_past_poll(self):
+        """
+        Polls with a pub_date in the past should be displayed on the index page.
+        """
+        create_poll(question="Past poll.", days=-30)
+        response = self.client.get(reverse('polls:index'))
+        self.assertQuerysetEqual(
+            response.context['poll_list'],
+            ['<Poll: Past poll.>']
+        )
+
+    def test_index_view_with_a_future_poll(self):
+        """
+        Polls with a pub_date in the future should not be displayed on the
+        index page.
+        """
+        create_poll(question="Future poll.", days=30)
+        response = self.client.get(reverse('polls:index'))
+        self.assertContains(response, "No polls are available.", status_code=200)
+        self.assertQuerysetEqual(response.context['poll_list'], [])
+
+    def test_index_view_with_future_poll_and_past_poll(self):
+        """
+        Even if both past and future polls exist, only past polls should be
+        displayed.
+        """
+        create_poll(question="Past poll.", days=-30)
+        create_poll(question="Future poll.", days=30)
+        response = self.client.get(reverse('polls:index'))
+        self.assertQuerysetEqual(
+            response.context['poll_list'],
+            ['<Poll: Past poll.>']
+        )
+
+    def test_index_view_with_two_past_polls(self):
+        """
+        The polls index page may display multiple polls.
+        """
+        create_poll(question="Past poll 1.", days=-30)
+        create_poll(question="Past poll 2.", days=-5)
+        response = self.client.get(reverse('polls:index'))
+        self.assertQuerysetEqual(
+            response.context['poll_list'],
+             ['<Poll: Past poll 2.>', '<Poll: Past poll 1.>']
+        )
+
