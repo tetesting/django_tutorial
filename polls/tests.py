3d04731d3414de9b90885d7b64551247194f7b8c
tetesting <tetestingtesting@gmail.com>
2013-10-14 20:46:58 -0400

    add a unittest ; fix bug in Poll model

--- polls/tests.py
+++ polls/tests.py
@@ -1,16 +1,17 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
 from django.test import TestCase
 
+from django.utils import timezone
+import datetime
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
+from polls.models import Poll
+
+
+class PollMethodTests(TestCase):
+    
+    def test_was_published_recently_with_future_poll(self):
         """
-        Tests that 1 + 1 always equals 2.
+        was_published_recently() should return False for polls whose
+        pub_date is in the future
         """
-        self.assertEqual(1 + 1, 2)
+        future_poll = Poll(pub_date=timezone.now() + datetime.timedelta(days=30))
+        self.assertEqual(future_poll.was_published_recently(), False)
